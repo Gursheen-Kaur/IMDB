9a92f83479b9b70cd384132fc05d8c5a00d37346
Gursheen-Kaur <gursheen.kaur@nineleaps.com>
2023-02-27 12:41:43 +0530

    Updating dependencies,creating movie entity

---
+++ src/main/java/rating/Movie.java
@@ -0,0 +1,68 @@
+package rating;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+//entity for springboot if we replace entity with model then it will store in database in the form of
+
+@Entity
+
+public class Movie {
+    @Id //setting primary key
+    @GeneratedValue(strategy=GenerationType.AUTO)   //auto generated value for integer id
+    private int id;
+    private String movieName;
+    private String movieDescription;
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id=" + id +
+                ", movieName='" + movieName + '\'' +        // IN THE FORM OF JSON
+                ", movieDescription='" + movieDescription + '\'' +
+                '}';
+    }
+
+    public Movie()
+    {
+        this.id=0;
+        this.movieName="";
+        this.movieDescription="";
+    }
+
+    public Movie(int id, String movieName, String movieDescription) {
+        // This keyword is refer to the current context of java class class
+        this.id = id;
+        this.movieName = movieName;
+        this.movieDescription = movieDescription;
+    }
+
+    // Getters are for getting the details and Setters are for assigning the details to the variables.
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getMovieDescription() {
+        return movieDescription;
+    }
+
+    public void setMovieDescription(String movieDescription) {
+        this.movieDescription = movieDescription;
+    }
+
+
+}
